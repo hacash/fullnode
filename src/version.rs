0060ebe19b03e73ae0520bfba2c289f503d6d80b
jojoin <2978963+jojoin@users.noreply.github.com>
2025-03-01 10:36:07 +0800

    up

--- src/version.rs
+++ src/version.rs
@@ -1,7 +1,7 @@
 
 
-const HACASH_NODE_VERSION: &str    = "0.2.9";
-const HACASH_NODE_BUILD_TIME: &str = "2025/02/26(1)";
+const HACASH_NODE_VERSION: &str    = "0.2.10";
+const HACASH_NODE_BUILD_TIME: &str = "2025/03/01(1)";
 #[allow(unused)]
 const HACASH_STATE_DB_UPDT: u32    = 1;
 
